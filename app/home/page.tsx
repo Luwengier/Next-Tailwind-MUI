c6096b4559560cfb2fb15b0370b8ce054d0ad1c3
Luewngier <g217881111@gmail.com>
2023-07-05 19:25:05 +0800

    feat: add MUI demo page

---
+++ app/home/page.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+// import { IBM_Plex_Sans } from 'next/font/google'
+
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import Button, { buttonClasses } from '@mui/base/Button'
+import { styled } from '@mui/system'
+
+import { ThemeProvider } from '../../features/theme'
+
+// const ibm = IBM_Plex_Sans({ subsets: ['latin'], weight: ['300', '500', '700'] })
+
+const Home = () => {
+  return (
+    <ThemeProvider>
+      123
+      <Box
+        sx={{
+          p: 3,
+          width: '100%',
+          height: '100vh',
+        }}
+      >
+        <Typography variant="h3" color="secondary">
+          I&apos;m Home Page.
+        </Typography>
+        456
+        <Stack spacing={2} direction="row">
+          <CustomButton>Button</CustomButton>
+          <CustomButton disabled>Disabled</CustomButton>
+        </Stack>
+      </Box>
+    </ThemeProvider>
+  )
+}
+
+const blue = {
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+}
+
+const grey = {
+  100: '#eaeef2',
+  300: '#afb8c1',
+  900: '#24292f',
+}
+
+const CustomButton = styled(Button)(
+  ({ theme }) => `
+  font-weight: bold;
+  font-size: 0.875rem;
+  background-color: ${blue[500]};
+  padding: 12px 24px;
+  border-radius: 12px;
+  color: white;
+  transition: all 150ms ease;
+  cursor: pointer;
+  border: none;
+  box-shadow: 0px 4px 30px ${
+    theme.palette.mode === 'dark' ? grey[900] : grey[100]
+  };
+
+  &:hover {
+    background-color: ${blue[600]};
+  }
+
+  &.${buttonClasses.active} {
+    background-color: ${blue[700]};
+  }
+
+  &.${buttonClasses.focusVisible} {
+    box-shadow: 0 3px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
+    outline: none;
+  }
+
+  &.${buttonClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+  `
+)
+
+export default Home
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300', '500', '700'],
+  variable: '--font-roboto',
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${roboto.className} ${inter.className}`}>
+      <body>{children}</body>
     </html>
   )
 }
