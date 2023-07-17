8baf16523351fc7b5275c5adcfcee39327dbd8a7
Luewngier <g217881111@gmail.com>
2023-07-17 17:28:28 +0800

    feat: add secret config of next-auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,39 @@
 import NextAuth, { AuthOptions } from 'next-auth'
-import Credentials from 'next-auth/providers/credentials'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 const authOptions: AuthOptions = {
   providers: [
-    Credentials({
-      name: 'credentials',
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' },
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
       },
-      async authorize(credentials) {
-        const user = { id: '1', name: 'Wengier', email: 'g217881111@gmail.com' }
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
 
         if (user) {
+          // Any object returned will be saved in `user` property of the JWT
           return user
         } else {
+          // If you return null then an error will be displayed advising the user to check their details.
           return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       },
     }),
   ],
+  pages: {
+    signIn: '/signin',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 }
 
 const handler = NextAuth(authOptions)
